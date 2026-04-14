5c57b02f185c27e5fcb8e55aa78dd760212ae6e6
michioxd <michio.haiyaku@gmail.com>
2026-04-14 14:08:45 +0700

    cleanup

--- src/resource.h
+++ src/resource.h
@@ -1,15 +1,34 @@
-#pragma once
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by foo_opensubsonic.rc
+//
+#define VS_VERSION_INFO                 1
+#define IDD_OPEN_SUBSONIC_PREFERENCES   201
+#define IDC_OS_SERVER_LIST              2001
+#define IDC_OS_SERVER_ADD               2002
+#define IDC_OS_SERVER_DELETE            2003
+#define IDC_OS_SERVER_NAME              2004
+#define IDC_OS_SERVER_URL               2005
+#define IDC_OS_LOCAL_SERVER_URL         2006
+#define IDC_OS_USERNAME                 2007
+#define IDC_OS_PASSWORD                 2008
+#define IDC_OS_CLIENT_NAME              2009
+#define IDC_OS_ALLOW_INSECURE_TLS       2010
+#define IDC_OS_SYNC_NOW                 2011
+#define IDC_OS_CLEAR_CACHE              2012
+#define IDC_OS_CACHE_ARTWORK            2013
+#define IDC_OS_CLEAR_ARTWORK_CACHE      2014
+#define IDC_OS_GITHUB_LINK              2015
+#define IDC_OS_VERSION_TEXT             2016
+#define IDC_STATIC                      -1
 
-#define IDD_OPEN_SUBSONIC_PREFERENCES 201
-
-#define IDC_OS_SERVER_URL 2001
-#define IDC_OS_USERNAME 2002
-#define IDC_OS_PASSWORD 2003
-#define IDC_OS_CLIENT_NAME 2005
-#define IDC_OS_ALLOW_INSECURE_TLS 2006
-#define IDC_OS_SYNC_NOW 2007
-#define IDC_OS_CLEAR_CACHE 2008
-#define IDC_OS_CACHE_ARTWORK 2009
-#define IDC_OS_CLEAR_ARTWORK_CACHE 2010
-#define IDC_OS_GITHUB_LINK 2012
-#define IDC_OS_VERSION_TEXT 2013
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
--- src/vfs.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace subsonic::vfs {
-
-inline constexpr size_t k_seek_backbuffer_size = 512 * 1024;
-
-} // namespace subsonic::vfs
