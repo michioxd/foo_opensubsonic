69a8cf70436b2175c3e5835d4a3d320321f0aa97
michioxd <michio.haiyaku@gmail.com>
2026-04-18 21:32:40 +0700

    0.2.2

--- src/component_info.h
+++ src/component_info.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define OPENSUBSONIC_COMPONENT_NAME "foo_opensubsonic"
-#define OPENSUBSONIC_COMPONENT_VERSION "0.2.1"
+#define OPENSUBSONIC_COMPONENT_VERSION "0.2.2"
 #define OPENSUBSONIC_COMPONENT_ABOUT "OpenSubsonic plugin for foobar2000\n"
 #define OPENSUBSONIC_COMPONENT_REPOSITORY_URL                                  \
 	"https://github.com/michioxd/foo_opensubsonic"
